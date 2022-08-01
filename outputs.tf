9e76a5ec7fcd79b88948e26b0c3594abf9e20839
andrii-lishchuk <110128282+andrii-lishchuk@users.noreply.github.com>
2022-08-02 01:05:19 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "my_server_ip" {
-  value = aws_eip.my_static_ip.public_ip
-}
-
-output "my_instance_id" {
-  value = aws_instance.my_server.id
-}
-
-output "my_sg_id" {
-  value = aws_security_group.my_server.id
+output "web_public_ip" {
+  value = aws_eip.web.public_ip
 }

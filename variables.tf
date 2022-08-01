b6895f9c3062f08d997daed04cd638a4518b42af
andrii-lishchuk <110128282+andrii-lishchuk@users.noreply.github.com>
2022-08-02 01:05:53 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,36 +1,12 @@
-
-variable "region" {
-  description = "Please Enter AWS Region to deploy Server"
+variable "server_name" {
+  description = "Name for WebServer"
   type        = string
-  default     = "ca-central-1"
+  default     = "demo"
 }
 
-variable "instance_type" {
-  description = "Enter Instance Type"
+
+variable "server_size" {
+  description = "Server Size for WebServer"
   type        = string
-  default     = "t3.small"
-}
-
-
-variable "allow_ports" {
-  description = "List of Ports to open for server"
-  type        = list
-  default     = ["80", "443", "22", "8080"]
-}
-
-variable "enable_detailed_monitoring" {
-  type    = bool
-  default = false
-}
-
-
-variable "common_tags" {
-  description = "Common Tags to apply to all resources"
-  type        = map
-  default = {
-    Owner       = "Denis Astahov"
-    Project     = "Phoenix"
-    CostCenter  = "12345"
-    Environment = "development"
-  }
+  default     = "t3.micro"
 }
